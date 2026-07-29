7493f5b21460ed821d33397029862b9eb8f55e52
miwe-max <miwe@byupathway.edu>
2026-07-29 16:08:20 +0100

    Added edit category functionality

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -1,6 +1,6 @@
-using Microsoft.EntityFrameworkCore;
 using cse325_finalproject.Data;
 using cse325_finalproject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace cse325_finalproject.Services;
 
@@ -37,4 +37,18 @@ public class CategoryService
             await _context.SaveChangesAsync();
         }
     }
+public async Task UpdateCategoryAsync(Category category)
+{
+    var existingCategory = await _context.Categories
+        .FirstOrDefaultAsync(c => c.Id == category.Id);
+
+    if (existingCategory != null)
+    {
+        existingCategory.Name = category.Name;
+
+        existingCategory.Description = category.Description;
+
+        await _context.SaveChangesAsync();
+    }
+}
 }
